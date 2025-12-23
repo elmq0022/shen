db03a0def434398187840373287831f33278c0a1
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-12-23 05:20:36 -0600

    adds shen_group table migrations

---
+++ db/migrations/000003_shen_group.up_table.sql
@@ -0,0 +1,13 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS shen_group (
+    id serial PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    active BOOLEAN NOT NULL DEFAULT true,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE INDEX idx_group_name ON shen_group(name);
+
+COMMIT;
